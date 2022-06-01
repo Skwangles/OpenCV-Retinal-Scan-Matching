0abf11b4a292d9d1c7710512b2a4b34dcffc9aa2
Skwangles <tree_house@hotmail.co.nz>
2022-06-01 21:44:49 +1200

    sample item

--- src/com/skwangles/HelloCV.java
+++ src/com/skwangles/HelloCV.java
@@ -3,17 +3,17 @@ import org.opencv.core.*;
 import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
 
-import static org.opencv.core.Core.BORDER_DEFAULT;
-import static org.opencv.core.Core.convertScaleAbs;
+import static org.opencv.core.Core.*;
 import static org.opencv.core.CvType.CV_16S;
 import static org.opencv.core.CvType.CV_64F;
-import static org.opencv.highgui.HighGui.imshow;
-import static org.opencv.highgui.HighGui.waitKey;
+import static org.opencv.highgui.HighGui.*;
+import static org.opencv.imgcodecs.Imgcodecs.imwrite;
 import static org.opencv.imgproc.Imgproc.*;
 
 import org.opencv.imgproc.*;
 import org.opencv.imgcodecs.*;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -30,10 +30,6 @@ public class HelloCV
 {
     public static void main(String[] args)
     {
-        int kernel_size = 3;
-        int scale = 1;
-        int delta = 0;
-        int ddepth = CV_16S;
         // load the OpenCV native library
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
 
@@ -45,43 +41,73 @@ public class HelloCV
         // prepare to convert a RGB image in gray scale
         String location = "RIDB/IM000001_1.jpg";
         String location2 = "RIDB/IM000002_2.jpg";
-        String location3 = "RIDB/IM000003_1.jpg";
         System.out.println("Convert the image at " + location + " in gray scale... ");
         // get the jpeg image from the internal resource folder
-        Mat srcBase = Imgcodecs.imread(location);
-        Mat srcTest1 = Imgcodecs.imread(location2);
-        Mat srcTest2 = Imgcodecs.imread(location3);
+        Mat src1 = Imgcodecs.imread(location);
+        Mat src2 = Imgcodecs.imread(location2);
 
-        if (srcBase.empty() || srcTest1.empty() || srcTest2.empty()) {
+        if (src1.empty() || src2.empty()) {
             System.err.println("Cannot read the images");
             System.exit(0);
         }
-        Mat hsvBase = new Mat(), hsvTest1 = new Mat(), hsvTest2 = new Mat();
-        Imgproc.cvtColor( srcBase, hsvBase, Imgproc.COLOR_BGR2HSV );
-        Imgproc.cvtColor( srcTest1, hsvTest1, Imgproc.COLOR_BGR2HSV );
-        Imgproc.cvtColor( srcTest2, hsvTest2, Imgproc.COLOR_BGR2HSV );
-        int hBins = 50, sBins = 60;
-        int[] histSize = { hBins, sBins };
-        // hue varies from 0 to 179, saturation from 0 to 255
-        float[] ranges = { 0, 180, 0, 256 };
-        // Use the 0-th and 1-st channels
-        int[] channels = { 0, 1 };
-        Mat histBase = new Mat(), histTest1 = new Mat(), histTest2 = new Mat();
-        List<Mat> hsvBaseList = Arrays.asList(hsvBase);
-        Imgproc.calcHist(hsvBaseList, new MatOfInt(channels), new Mat(), histBase, new MatOfInt(histSize), new MatOfFloat(ranges), false);
-        Core.normalize(histBase, histBase, 0, 1, Core.NORM_MINMAX);
-        List<Mat> hsvTest1List = Arrays.asList(hsvTest1);
-        Imgproc.calcHist(hsvTest1List, new MatOfInt(channels), new Mat(), histTest1, new MatOfInt(histSize), new MatOfFloat(ranges), false);
-        Core.normalize(histTest1, histTest1, 0, 1, Core.NORM_MINMAX);
-        List<Mat> hsvTest2List = Arrays.asList(hsvTest2);
-        Imgproc.calcHist(hsvTest2List, new MatOfInt(channels), new Mat(), histTest2, new MatOfInt(histSize), new MatOfFloat(ranges), false);
-        Core.normalize(histTest2, histTest2, 0, 1, Core.NORM_MINMAX);
-        for( int compareMethod = 0; compareMethod < 4; compareMethod++ ) {
-            double baseBase = Imgproc.compareHist( histBase, histBase, compareMethod );
-            double baseTest1 = Imgproc.compareHist( histBase, histTest1, compareMethod );
-            double baseTest2 = Imgproc.compareHist( histBase, histTest2, compareMethod );
-            System.out.println("Method " + compareMethod + " Perfect, Base-Test(1), Base-Test(2) : " + baseBase + " / " + baseTest1 + " / " + baseTest2);
-        }
+
+        namedWindow("Original");
+        imshow("Original", src1);
+        namedWindow("Original2");
+        imshow("Original2", src2);
+        waitKey();
+
+        //Make grayscale
+        cvtColor(src1, src1, COLOR_BGR2GRAY);
+        cvtColor(src2, src2, COLOR_BGR2GRAY);
+        GaussianBlur(src1, src1, new Size(3,3), 1);
+        GaussianBlur(src2, src2, new Size(3,3), 1);
+        namedWindow("blur");
+        imshow("blur", src1);
+        namedWindow("blur2");
+        imshow("blur2", src2);
+        waitKey();
+        Mat mask = src1;
+        threshold(src1, mask, 10, 255, THRESH_BINARY);//Get mask area to focus on only the retina
+
+
+//
+//        Mat mask1 = Mat.zeros(src1.rows(), src1.cols(), src1.type());
+//        Mat mask2 = Mat.zeros(src2.rows(), src2.cols(), src2.type());
+//        threshold(src1, mask1, 10, 255, THRESH_BINARY);
+//        threshold(src2, mask2, 10, 255, THRESH_BINARY);
+//        src1 = src1-mask1;
+//        adaptiveThreshold(src1, mask1 , 255,ADAPTIVE_THRESH_GAUSSIAN_C, THRESH_BINARY,7,2);
+//        adaptiveThreshold(src2, mask2, 255,ADAPTIVE_THRESH_GAUSSIAN_C, THRESH_BINARY,7,2);
+        src1.convertTo(src1, -1, 1.7, 0);
+        src2.convertTo(src2, -1, 1.7, 0);
+        //blur(src1, src1, new Size(3,3));
+
+
+
+        namedWindow("contrast");
+        imshow("contrast", src1);
+
+        namedWindow("contrast2");
+        imshow("contrast2", src2);
+        waitKey();
+
+        Canny(src1, src1, 30, 70);
+        Canny(src2, src2, 30, 70);
+
+
+
+        namedWindow("Final");
+        imshow("Final", src1);
+
+        namedWindow("Final2");
+        imshow("Final2", src2);
+
+        waitKey();
         System.out.println("Done!");
     }
+
+
+
+
 }
