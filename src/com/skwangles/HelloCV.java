9a72af4e6b14a107cd560d3d34f2ad62a6837c42
Skwangles <tree_house@hotmail.co.nz>
2022-06-04 12:09:29 +1200

    Basic version - not working accurately

--- src/com/skwangles/HelloCV.java
+++ src/com/skwangles/HelloCV.java
@@ -1,7 +1,12 @@
 package com.skwangles;
 import org.opencv.core.*;
+import org.opencv.highgui.HighGui;
 import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.opencv.core.Core.*;
 import static org.opencv.core.CvType.CV_16S;
@@ -10,12 +15,6 @@ import static org.opencv.highgui.HighGui.*;
 import static org.opencv.imgcodecs.Imgcodecs.imwrite;
 import static org.opencv.imgproc.Imgproc.*;
 
-import org.opencv.imgproc.*;
-import org.opencv.imgcodecs.*;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 /**
  * A simple class that demonstrates/tests the usage of the OpenCV library in
@@ -30,13 +29,16 @@ public class HelloCV
 {
     public static void main(String[] args)
     {
+        //Pipeline
+        //Remember to clean up the image with smoothing filters
+        // • Choose a sensible colour channel or space to work in
+        // • Possible contrast equalization to ensure that variances in illumination don’t mess up the results
+        // • Pick image transformations (e.g. edges, thresholded, etc) and features
+        // • Hopefully get something that works
+
+
         // load the OpenCV native library
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-
-        // create and print on screen a 3x3 identity matrix
-        System.out.println("Create a 3x3 identity matrix...");
-        Mat mat = Mat.eye(3, 3, CvType.CV_8UC1);
-        System.out.println("mat = " + mat.dump());
 
         // prepare to convert a RGB image in gray scale
         String location = "RIDB/IM000001_1.jpg";
@@ -57,56 +59,63 @@ public class HelloCV
         imshow("Original2", src2);
         waitKey();
 
-        //Make grayscale
         cvtColor(src1, src1, COLOR_BGR2GRAY);
         cvtColor(src2, src2, COLOR_BGR2GRAY);
-        GaussianBlur(src1, src1, new Size(3,3), 1);
-        GaussianBlur(src2, src2, new Size(3,3), 1);
-        namedWindow("blur");
-        imshow("blur", src1);
-        namedWindow("blur2");
-        imshow("blur2", src2);
-        waitKey();
-        Mat mask = src1;
-        threshold(src1, mask, 10, 255, THRESH_BINARY);//Get mask area to focus on only the retina
+        //Compare beforhand to after
+        equalizeHist(src1, src1);
+        equalizeHist(src2, src2);
+        //printSrcs(src1, src2);
+
+        GaussianBlur(src1, src1, new Size(7,7), 15);
+        GaussianBlur(src2, src2, new Size(7,7), 15);
+
+        printSrcs(src1, src2);
+
+        Mat mask1 = Mat.zeros(src1.rows(), src1.cols(), src1.type());
+        Mat mask2 = Mat.zeros(src2.rows(), src2.cols(), src2.type());
+
+        adaptiveThreshold(src1, mask1 , 255,ADAPTIVE_THRESH_GAUSSIAN_C, THRESH_BINARY_INV,7,4);
+        adaptiveThreshold(src2, mask2, 255,ADAPTIVE_THRESH_GAUSSIAN_C, THRESH_BINARY_INV,7,4);
 
 
-//
-//        Mat mask1 = Mat.zeros(src1.rows(), src1.cols(), src1.type());
-//        Mat mask2 = Mat.zeros(src2.rows(), src2.cols(), src2.type());
-//        threshold(src1, mask1, 10, 255, THRESH_BINARY);
-//        threshold(src2, mask2, 10, 255, THRESH_BINARY);
-//        src1 = src1-mask1;
-//        adaptiveThreshold(src1, mask1 , 255,ADAPTIVE_THRESH_GAUSSIAN_C, THRESH_BINARY,7,2);
-//        adaptiveThreshold(src2, mask2, 255,ADAPTIVE_THRESH_GAUSSIAN_C, THRESH_BINARY,7,2);
-        src1.convertTo(src1, -1, 1.7, 0);
-        src2.convertTo(src2, -1, 1.7, 0);
-        //blur(src1, src1, new Size(3,3));
+        int kernelSize = 1;
+        int elementType = Imgproc.CV_SHAPE_RECT;
+        Mat element = Imgproc.getStructuringElement(elementType, new Size(2 * kernelSize + 1, 2 * kernelSize + 1), new Point(kernelSize, kernelSize));
+        dilate(mask1, mask1, element );
+        dilate(mask2, mask2, element );
+        //printSrcs(mask1, mask2);
+
+        GaussianBlur(mask1, mask1, new Size(11,11), 24);
+        GaussianBlur(mask2, mask2, new Size(11,11), 24);
+        //printSrcs(mask1, mask2);
+
+        kernelSize = 2;
+        element = Imgproc.getStructuringElement(elementType, new Size(2 * kernelSize + 1, 2 * kernelSize + 1), new Point(kernelSize, kernelSize));
+        erode(mask1, mask1, element );
+        erode(mask2, mask2, element );
+        printSrcs(mask1, mask2);
 
 
+        List<Mat> hsvBaseList = Arrays.asList(mask1);
+        Imgproc.calcHist(hsvBaseList, new MatOfInt(0), new Mat(), mask1, new MatOfInt(new int[]{10}), new MatOfFloat(0, 256), false);
+        Core.normalize(mask1, mask1, 0, 1, Core.NORM_MINMAX);
 
-        namedWindow("contrast");
-        imshow("contrast", src1);
+        List<Mat> hsvBaseList2 = Arrays.asList(mask2);
+        Imgproc.calcHist(hsvBaseList2, new MatOfInt(0), new Mat(), mask2, new MatOfInt(new int[]{10}), new MatOfFloat(0, 256), false);
+        Core.normalize(mask2, mask2, 0, 1, Core.NORM_MINMAX);
 
-        namedWindow("contrast2");
-        imshow("contrast2", src2);
-        waitKey();
-
-        Canny(src1, src1, 30, 70);
-        Canny(src2, src2, 30, 70);
-
-
-
-        namedWindow("Final");
-        imshow("Final", src1);
-
-        namedWindow("Final2");
-        imshow("Final2", src2);
-
-        waitKey();
+        double output = compareHist(mask1, mask2, 0); //Chi-square comparison method
+        System.out.println(output);
         System.out.println("Done!");
     }
 
+    public static void printSrcs(Mat src1, Mat src2){
+        namedWindow("src2");
+        imshow("src2", src2);
+        namedWindow("src1");
+        imshow("src1", src1);
+        waitKey();
+    }
 
 
 
